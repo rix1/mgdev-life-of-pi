ac37f7cebea63e18c6ac65dd8b8eed33b4a01ece
rix1 <rikardeide@gmail.com>
2015-05-15 14:07:23 +0200

    addded button controller

---
+++ Assets/Scripts/_buttonController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _buttonController : MonoBehaviour {
+
+	public void activateButtons(bool activate){
+		BoxCollider2D[] btnColliders = transform.GetComponentsInChildren<BoxCollider2D>();
+		
+		foreach (BoxCollider2D item in btnColliders)
+		{
+			item.enabled = activate;
+		}
+	}
+
+}
